0a19e5b32715e98f04d0419ba31f442f47a4f260
Saima Akhtar <saimaakhtar@gmail.com>
2020-03-29 02:20:11 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -33,8 +33,7 @@ def total_gross(source)
   while director_index < source.size do
   total_gross_of_earnings += gross_for_director( source[director_index] )
    director_index += 1
-  # Visit each key (i.e. director name), look up the value in the hash	  end
-  # returned by directors_totals, and add it to a running total. When done,	total_gross_of_earnings
+  
 end	end
   
 
