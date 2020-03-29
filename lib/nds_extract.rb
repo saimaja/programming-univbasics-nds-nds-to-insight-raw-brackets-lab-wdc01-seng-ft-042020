9de6c0c81b75cd5b4abac1b18450153bd659a94c
Saima Akhtar <saimaakhtar@gmail.com>
2020-03-29 02:17:57 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -11,8 +11,8 @@ end
 def gross_for_director(d)
   total = 0	
   index = 0	
-  total += d[:movies][index][:worldwide_gross]	   
-  index += 1	  
+    total += d[:movies][index][:worldwide_gross]	   
+    index += 1	  
   end
   total
 end
