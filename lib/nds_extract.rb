4f19ae23e9ffbe54aaaed1e62db4124c1b488096
Saima Akhtar <saimaakhtar@gmail.com>
2020-03-29 02:20:22 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -33,7 +33,8 @@ def total_gross(source)
   while director_index < source.size do
   total_gross_of_earnings += gross_for_director( source[director_index] )
    director_index += 1
-  
-end	end
+  end
+  total_gross_of_earnings
+end	
   
 
