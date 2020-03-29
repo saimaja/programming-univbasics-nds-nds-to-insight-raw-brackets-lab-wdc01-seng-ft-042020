f469ad99f2792bc21c6486b8a9e434927e59056d
Saima Akhtar <saimaakhtar@gmail.com>
2020-03-29 02:26:19 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,7 +4,7 @@ pp directors_database
 
 def directors_totals(nds)
   result = {}	  
- director_index = 0
+  director_index = 0
   while director_index < nds.length do
     current_director = nds[director_index][:name]
     result[current_director] = gross_for_director( nds[director_index] )
