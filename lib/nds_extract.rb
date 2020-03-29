998460a395acdea8c5b51ff4359a3721a5a98991
Saima Akhtar <saimaakhtar@gmail.com>
2020-03-29 02:23:47 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,34 +4,5 @@ pp directors_database
 
 def directors_totals(nds)
   result = {}	  
-  director_index = 0	  
-  result	  
-end
-
-def gross_for_director(d)
-  total = 0	
-  index = 0	
-    total += d[:movies][index][:worldwide_gross]	   
-    index += 1	  
-  end
  
-def list_of_directors(source)
-  director_index = 0
-  director_list = Array.new
-  while director_index < source.size do
-    director_list.push(source[director_index][:name])
-    director_index += 1
-  end
- 
-
-def total_gross(source)
-  total_gross_of_earnings = 0
-  director_index = 0	
-  while director_index < source.size do
-  total_gross_of_earnings += gross_for_director( source[director_index] )
-   director_index += 1
-  end
-  total_gross_of_earnings
-end	
-end
 
